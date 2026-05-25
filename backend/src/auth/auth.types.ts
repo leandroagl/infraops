42ab3c1dc56dd595bd842e6954dee11562e256d0
unknown <leandro.n.aguilera@gmail.com>
2026-05-24 23:25:36 -0300

    feat(auth): interface JwtPayload

---
+++ backend/src/auth/auth.types.ts
@@ -0,0 +1,10 @@
+import { UserRole } from '../users/user-role.enum';
+
+export interface JwtPayload {
+  sub: string;
+  email: string;
+  role: UserRole;
+  mustChangePassword: boolean;
+  iat?: number;
+  exp?: number;
+}
