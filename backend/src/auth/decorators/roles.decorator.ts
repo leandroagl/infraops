845a9609ba1344ab767dcfa39c13f998f10b07f8
unknown <leandro.n.aguilera@gmail.com>
2026-05-24 23:40:31 -0300

    feat(auth): JwtAuthGuard y decoradores @Roles @CurrentUser

---
+++ backend/src/auth/decorators/roles.decorator.ts
@@ -0,0 +1,6 @@
+import { SetMetadata } from '@nestjs/common';
+import { UserRole } from '../../users/user-role.enum';
+
+export const Roles = (
+  ...roles: UserRole[]
+): ReturnType<typeof SetMetadata> => SetMetadata('roles', roles);
