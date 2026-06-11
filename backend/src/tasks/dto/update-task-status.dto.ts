95e70c0c35af5da7482d09645746e43e9cf1a712
unknown <leandro.n.aguilera@gmail.com>
2026-06-10 21:38:38 -0300

    feat(tasks): UpdateTaskStatusDto requiere timeSpentMinutes al cerrar tarea

--- backend/src/tasks/dto/update-task-status.dto.ts
+++ backend/src/tasks/dto/update-task-status.dto.ts
@@ -1,7 +1,12 @@
-import { IsEnum } from 'class-validator';
+import { IsEnum, IsInt, Min, ValidateIf } from 'class-validator';
 import { TaskStatus } from '../task-status.enum';
 
 export class UpdateTaskStatusDto {
   @IsEnum(TaskStatus)
   status: TaskStatus;
+
+  @ValidateIf((o) => o.status === TaskStatus.DONE || o.status === TaskStatus.NOT_DONE)
+  @IsInt()
+  @Min(1)
+  timeSpentMinutes?: number;
 }
