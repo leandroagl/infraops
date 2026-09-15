39e90fd2b36c7414a8fd8656e57a54b7a1781ea1
unknown <leandro.n.aguilera@gmail.com>
2026-09-15 14:51:47 -0300

    feat(notifications): crear NotificationsConfigDialogComponent para configurar ticketera de vencimientos

--- frontend/src/app/features/notifications/notifications.module.ts
+++ frontend/src/app/features/notifications/notifications.module.ts
@@ -1,24 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { NotificationsRoutingModule } from './notifications-routing.module';
 import { NotificationsComponent } from './notifications.component';
+import { NotificationsConfigDialogComponent } from './config-dialog/notifications-config-dialog.component';
 
 @NgModule({
-  declarations: [NotificationsComponent],
+  declarations: [NotificationsComponent, NotificationsConfigDialogComponent],
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     MatButtonModule,
+    MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
+    MatMenuModule,
     MatProgressBarModule,
+    MatProgressSpinnerModule,
     MatSelectModule,
+    MatTooltipModule,
     NotificationsRoutingModule,
   ],
 })
